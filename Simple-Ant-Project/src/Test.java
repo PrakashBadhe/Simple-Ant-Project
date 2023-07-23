92419c9eb7ccab1df9ea691c38555059a0ef01e3
PrakashBadhe <prakash.badhe2023@gmail.com>
2023-07-23 21:42:52 +0530

    New Project created

---
+++ Simple-Ant-Project/src/Test.java
@@ -0,0 +1,12 @@
+
+ public class Test
+ {
+
+  public static void main(String args[])
+   {
+   System.out.println("Now Here it is running ");   
+    
+   System.out.println("\nThis is running testing here...!");   
+   }
+
+} 
