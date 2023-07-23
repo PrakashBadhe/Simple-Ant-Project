36b3eea53b29a9320782c7cc2e46f10862b6d148
PrakashBadhe <prakash.badhe2023@gmail.com>
2023-07-23 21:47:23 +0530

    Tests change

--- Simple-Ant-Project/src/Test.java
+++ Simple-Ant-Project/src/Test.java
@@ -4,7 +4,7 @@
 
   public static void main(String args[])
    {
-   System.out.println("Now Here it is running in Eclipse ");   
+   System.out.println("Here it is running in Eclipse ");   
     
    System.out.println("\nThis is running testing here...!");   
    }
